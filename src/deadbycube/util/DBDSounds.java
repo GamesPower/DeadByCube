eb77a4bceab96f34f1169ec95abc5778ebe12036
Fabien S <games.ultrapower@gmail.com>
2017-12-11 20:56:33 +0100

    Create a sounds register for Dead by Cube

---
+++ src/deadbycube/util/DBDSounds.java
@@ -0,0 +1,8 @@
+package deadbycube.util;
+
+public class DBDSounds {
+
+    public static final String KILLER_WRAITH_INVISIBILITY_ON = "killer.wraith.invisibility.on";
+    public static final String KILLER_WRAITH_INVISIBILITY_OFF = "killer.wraith.invisibility.off";
+
+}
