a4927d0f34f76a313008c1a7d4c056710b7e4649
Fabien S <games.ultrapower@gmail.com>
2017-12-11 23:16:33 +0100

    Added default walk speed & food level for survivor

--- src/deadbycube/player/survivor/Survivor.java
+++ src/deadbycube/player/survivor/Survivor.java
@@ -2,12 +2,13 @@ package deadbycube.player.survivor;
 
 import deadbycube.DeadByCube;
 import deadbycube.player.DbcPlayer;
-import deadbycube.player.PlayerActionHandler;
 import deadbycube.player.PlayerType;
-import deadbycube.player.killer.KillerActionHandler;
 import org.bukkit.entity.Player;
 
 public class Survivor extends DbcPlayer {
+
+    private static final float DEFAULT_WALK_SPEED = 0.2f;
+    private static final int DEFAULT_FOOD_LEVEL = 20;
 
     public Survivor(DeadByCube plugin, Player player) {
         super(plugin, player);
@@ -15,6 +16,8 @@ public class Survivor extends DbcPlayer {
 
     @Override
     public void init() {
+        player.setWalkSpeed(DEFAULT_WALK_SPEED);
+        player.setFoodLevel(DEFAULT_FOOD_LEVEL);
     }
 
     @Override
