6fcdbd1d51987e0bc9b136d0cf4adc133264f4cf
Fabien S <games.ultrapower@gmail.com>
2017-12-18 09:43:35 +0100

    Updated walk speed and optimize some stuffs

--- src/deadbycube/player/killer/power/PowerCartersSpark.java
+++ src/deadbycube/player/killer/power/PowerCartersSpark.java
@@ -1,17 +1,16 @@
 package deadbycube.player.killer.power;
 
+import deadbycube.audio.PlayerAudioManager;
 import deadbycube.player.killer.Killer;
 import deadbycube.util.DBDSounds;
 import deadbycube.util.MathUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class PowerCartersSpark extends Power {
-
-    private static final float WALK_SPEED = Killer.DEFAULT_WALK_SPEED;
-    private static final float CHARGING_WALK_SPEED = Killer.DEFAULT_WALK_SPEED + .5f;
 
     private static final int SHOCK_DISTANCE = 10;
     private static final int SHOCK_CHARGE_TIME = 30;
@@ -22,7 +21,7 @@ public class PowerCartersSpark extends Power {
     public PowerCartersSpark(Killer killer) {
         super(killer);
 
-        killer.getPlayer().setWalkSpeed(WALK_SPEED);
+        killer.setWalkSpeed(Killer.DEFAULT_WALK_SPEED);
     }
 
     @Override
@@ -33,6 +32,8 @@ public class PowerCartersSpark extends Power {
     @Override
     protected void onUse() {
         Player player = killer.getPlayer();
+        killer.setWalkSpeedModifier(3);
+
         World world = player.getWorld();
         Location location = player.getLocation();
         world.playSound(location, DBDSounds.KILLER_DOCTOR_CHARGE, 1, 1);
@@ -45,29 +46,31 @@ public class PowerCartersSpark extends Power {
         Player player = killer.getPlayer();
         World world = player.getWorld();
 
+        Location particleLocation = player.getLocation().add(0, .8, 0);
         for (int i = 0; i < (chargeTime / 2); i++)
-            world.spawnParticle(Particle.SMOKE_NORMAL, player.getLocation().add(0, .8, 0), 1, .2, .35, .2, 0);
+            world.spawnParticle(Particle.SMOKE_NORMAL, particleLocation, 1, .2, .35, .2, 0);
 
         if (++chargeTime >= SHOCK_CHARGE_TIME) {
-            world.playSound(player.getLocation(), DBDSounds.KILLER_DOCTOR_ATTACK_READY, 1, 1);
+            killer.setWalkSpeedModifier(1);
 
-            float distanceStep = .4f;
+            float distanceStep = .5f;
             float angleStep = 3.2f;
 
-            player.setWalkSpeed(CHARGING_WALK_SPEED);
+            Location soundLocation = player.getLocation();
+            world.playSound(soundLocation, DBDSounds.KILLER_DOCTOR_ATTACK_READY, 1, 1);
+            world.playSound(soundLocation, DBDSounds.KILLER_DOCTOR_ATTACK, 1f, 1f);
 
-            world.playSound(player.getLocation(), DBDSounds.KILLER_DOCTOR_ATTACK, 1f, 1f);
-
-            Location baseLocation = killer.getPlayer().getLocation();
             for (float distance = 1.5f; distance < SHOCK_DISTANCE; distance += distanceStep) {
                 for (double angle = -SHOCK_ANGLE; angle <= SHOCK_ANGLE; angle += angleStep) {
-                    Location location = baseLocation.clone().add(
-                            MathUtils.getDirection(baseLocation.getYaw() + angle, 0).multiply(distance)
+                    Location location = player.getLocation();
+                    location = location.add(
+                            MathUtils.getDirection(location.getYaw() + angle, 0).multiply(distance)
                     );
                     world.spawnParticle(Particle.CLOUD, location, 0, 1, 0, 1, 0);
                 }
             }
 
+            Bukkit.getScheduler().runTaskLater(killer.getPlugin(), () -> world.playSound(player.getLocation(), DBDSounds.KILLER_DOCTOR_LAUGH, 1, 1), 10);
             this.stopUpdate();
         }
     }
@@ -75,7 +78,7 @@ public class PowerCartersSpark extends Power {
     @Override
     protected void onStopUse() {
         this.chargeTime = 0;
-        killer.getPlayer().setWalkSpeed(WALK_SPEED);
+        killer.setWalkSpeedModifier(1);
     }
 
 }
