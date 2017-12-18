0aadb4c252add4ae6d59625bd2de85a9d0c14e41
Fabien S <games.ultrapower@gmail.com>
2017-12-18 09:42:47 +0100

    Added some sounds

--- src/deadbycube/player/killer/power/evilwithin/PowerEvilWithin.java
+++ src/deadbycube/player/killer/power/evilwithin/PowerEvilWithin.java
@@ -1,9 +1,11 @@
 package deadbycube.player.killer.power.evilwithin;
 
-import deadbycube.player.PlayerManager;
+import deadbycube.audio.DbcSounds;
+import deadbycube.audio.PlayerAudioManager;
 import deadbycube.player.killer.Killer;
 import deadbycube.player.killer.power.Power;
 import deadbycube.player.survivor.Survivor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 abstract class PowerEvilWithin extends Power {
@@ -23,13 +25,17 @@ abstract class PowerEvilWithin extends Power {
 
     @Override
     protected void onUse() {
+        Player player = killer.getPlayer();
+        PlayerAudioManager audioManager = killer.getAudioManager();
+        audioManager.playSound(DbcSounds.KILLER_SHAPE_STALK_ON, player.getLocation(), 1, 1);
+        audioManager.playSoundLater(DbcSounds.KILLER_SHAPE_STALK_ON, player.getLocation(), 1, 1, 15);
     }
 
     @Override
     protected void onUpdate() {
         Player player = killer.getPlayer();
 
-        for (Survivor survivor : PlayerManager.getSurvivors()) {
+        for (Survivor survivor : killer.getPlugin().getPlayerList().getSurvivors()) {
             if (player.hasLineOfSight(survivor.getPlayer())) {
                 stalked++;
                 this.onStalk();
@@ -40,6 +46,13 @@ abstract class PowerEvilWithin extends Power {
 
     @Override
     protected void onStopUse() {
+        Player player = killer.getPlayer();
+        PlayerAudioManager audioManager = killer.getAudioManager();
+        Bukkit.getScheduler().runTaskLater(killer.getPlugin(), () -> {
+            audioManager.playSound(DbcSounds.KILLER_SHAPE_STALK_OFF, player.getLocation(), 1, 1);
+            audioManager.playSoundLater(DbcSounds.KILLER_SHAPE_STALK_OFF, player.getLocation(), 1, 1, 15);
+        }, Math.min(0, stalked - 40));
+
     }
 
 }
